1f88c6550fdf20f57d5227d6b76c615f95a12143
bakkachi ayoub <ayoubbakkachi11@gmail.com>
2021-11-12 15:38:37 +0100

    challenge 1

---
+++ exercice1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+char nom[20],prenom[20],sexe[20];
+int age;
+char num_tele[20];
+printf("veuilles entrer votre nom");
+scanf("%s",&nom);
+printf("veuiller entrer votre prenom");
+scanf("%s",&prenom);
+printf("veuiller entrer votrer age");
+scanf("%d" ,&age);
+printf("veuiller entrer votre sexe");
+scanf("%s",&sexe);
+printf("veuiller entrer votre num");
+scanf("%s",&num_tele);
+return 0;
+}
