2e57ac87d018d8caabf5a25f08e838ef1c1d0f9b
bakkachi ayoub <ayoubbakkachi11@gmail.com>
2021-11-12 16:20:42 +0100

    challenge 2

---
+++ exercice2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	float fahrenheit,celsius;
+	printf("entrer la temperature en fahrenheit ");
+	scanf("%f",&fahrenheit);
+	celsius=(fahrenheit*-32)/1,8;
+	
+	printf("la resulte en degre celsius est %f: ",celsius);
+	
+	return 0;
+    
+
+
+
+}
